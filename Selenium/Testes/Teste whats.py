18c810865ad6c0a4c71aa8b456bcf601d57d9481
RafaelQSantos-RQS <rafael.qsantos33@gmail.com>
2023-02-06 08:02:18 -0300

    modified:   Selenium/Testes/Teste whats.py

--- Selenium/Testes/Teste whats.py
+++ Selenium/Testes/Teste whats.py
@@ -5,9 +5,11 @@ texto = "Texto"
 telefone = "numero"
 link = f'https://web.whatsapp.com/send?phone=55{telefone}&text={texto}'
 
-with sync_playwright() as p:
-    navegador = p.chromium.launch(headless=False) # headless
-    pagina = navegador.new_page()
-    pagina.goto(link)
-    pagina.keyboard.down('Enter')
-    time.sleep(10)
+def Acesso(link):
+    with sync_playwright() as p:
+        navegador = p.chromium.launch(headless=False) # headless
+        pagina = navegador.new_page()
+        pagina.goto(link)
+        pagina.keyboard.down('Enter')
+
+Acesso(link)
